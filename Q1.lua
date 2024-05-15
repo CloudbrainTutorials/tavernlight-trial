0cff5d82348974925ad4a7534ef5c995a1728636
Cloudbrain_ <60580530+CloudbrainTutorials@users.noreply.github.com>
2024-05-15 07:38:28 -0400

    Updated trial scripts
    Replaced the original scripts with the updated ones.
    
    Added some comments in all of them guessing what some functions do.
    
    Added an else-condition to Q1 to allow the function to return false when the slotId was already empty.
    
    Made a small correction in Q2 where it seemed resultId was accidentally called result when being used.
    
    Renamed the function in Q3 based on my understanding of what it was doing because the name wasn't descriptive enough. Also made the player variable local to match the party variable and moved Player(membername) into its own variable because it's used multiple times.
    
    Added includes and the std namespace to Q4 to load the necessary files in preprocessing. Simplified the section that searched for the player by only using the loadByPlayerName function so we don't look for the player twice.

--- Q1.lua
+++ Q1.lua
@@ -1,10 +1,20 @@
-local function releaseStorage(player)
-    player:setStorageValue(1000, -1)
+
+-- Seems to release whatever is stored in slot id 1000
+-- Added slotId parameter to match what as in onLogout method
+-- Otherwise it we could remove the second parameter of the addEvent call as it doesn't seem to used
+-- Added the new parameter in the first position because it is given before 'player' in the call to addEvent
+local function releaseStorage(slotId, player)
+    player:setStorageValue(slotId, -1)
 end
-    
+
+-- Confirms if releasing the object at slot 1000 was successful on logout
 function onLogout(player)
     if player:getStorageValue(1000) == 1 then
         addEvent(releaseStorage, 1000, player)
+        -- Moved return statement into here so I could add an else statement
+        return true
+    -- Added an else condition for if the storage value isn't 1
+    else
+        return false
     end
-    return true
 end
--- Q3.lua
+++ Q3.lua
@@ -1,10 +1,19 @@
-function do_sth_with_PlayerParty(playerId, membername)
-    player = Player(playerId)
+-- Renamed the function for clarity based on what the function seems to be doing
+-- Seems like this method gets the player and target party member, searches for that party member,
+-- and removes them from the player's party
+function removeMemberFromParty(playerId, membername)
+    -- Made this local because we're only using it here
+    local player = Player(playerId)
     local party = player:getParty()
+    -- Defined member name as a variable because it is being used multiple times; makes it easier to change later
+    -- Made an assumption that you can pass a name into Player() as well as an ID
+    local targetMember = Player(membername)
     
+    -- It seems like getMembers() would return the PID and name, but the function takes the member name
+    -- If that's the case, it may be better to check against the PID instead of the name if names aren't unique
     for k,v in pairs(party:getMembers()) do
-        if v == Player(membername) then
-            party:removeMember(Player(membername))
+        if v == targetMember then
+            party:removeMember(targetMember)
         end
     end
 end
